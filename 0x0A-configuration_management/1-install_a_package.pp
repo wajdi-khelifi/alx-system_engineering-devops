f6d298e2ba6cc4956b88368b94466a9e44ead42c
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-19 18:49:00 +0000

    Install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,18 @@
 #!/usr/bin/pup
 # Using puppet install flask
 
-package {'flask':
+package {'python3-pip':
+  ensure   => installed,
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  = Package['python3-pip'],
 }
