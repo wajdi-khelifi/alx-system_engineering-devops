92266aa35208f74bd8669bd32b0d4ae6e3f5838c
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-01-21 16:39:31 +0000

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Make change to config file using puppet
+
+include stdlib
+
+file_line { 'Refuse to authenticate using password':
+  ensure => present,
+  path   => 'etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => 'etc/ssh/ssh_config',
+  line   => 'IdentifyFile ~/.ssh/school',
+}
