c039c539076ebcf9adfe849ee703ff4cf5e1762c
wajdi khelifi <133996419+wajdi-khelifi@users.noreply.github.com>
2024-02-10 18:32:12 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,7 +7,7 @@ file_line { 'aaaaa':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/wajdi-khelifi permanent;',
 }
 
 file { '/var/www/html/index.html':
