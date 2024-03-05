b0363dc4d0bf4e466368fef1faa2ebb90433137c
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-03-05 15:43:30 +0100

    Advanced api done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/khelifi_wajdi)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 404 or response.is_redirect:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
