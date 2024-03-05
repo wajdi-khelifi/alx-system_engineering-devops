08e25800f811e22cf622905d5ac456a0b93e49bc
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-03-05 15:59:51 +0100

    updated and fix problem

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
-"""
-Script to print hot posts on a given Reddit subreddit.
-"""
-
-import requests
+import requests as r
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    # Construct the URL for the subreddit's hot posts in JSON format
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
-    # Define headers for the HTTP request, including User-Agent
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-
-    # Define parameters for the request, limiting the number of posts to 10
-    params = {
-        "limit": 10
-    }
-
-    # Send a GET request to the subreddit's hot posts page
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-
-    # Check if the response status code indicates a not-found error (404)
-    if response.status_code == 404:
-        print("None")
-        return
-
-    # Parse the JSON response and extract the 'data' section
-    results = response.json().get("data")
-
-    # Print the titles of the top 10 hottest posts
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """
+    queries the Reddit API and prints the titles of the first 10 hot posts
+    """
+    headers = {"User-Agent": "Frocuts"}
+    endpoint = "http://reddit.com/r/{}/hot.json?limit=10"
+    subs = r.get(endpoint.format(subreddit), headers=headers)
+    if subs.status_code != 200:
+        print(None)
+        return 0
+    subs = subs.json()
+    if subs["kind"] == "Listing":
+        for data in subs["data"]["children"]:
+            print(data["data"]["title"])
+    else:
+        print(None)
