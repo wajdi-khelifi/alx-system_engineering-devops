74b5cd86b52634b3d6d2d67e386785cb50ea6b00
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-03-05 15:51:16 +0100

    updated and fix problem

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,7 +5,6 @@ Script that queries subscribers on a given Reddit subreddit.
 import requests
 
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
