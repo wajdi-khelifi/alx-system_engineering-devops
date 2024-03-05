88167f076882d0c13ab5747cd9796ff84b541650
wajdi-khelifi <khelifi.wajdi@hotmail.com>
2024-03-05 15:52:07 +0100

    updated and fix problem

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code == 404:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
